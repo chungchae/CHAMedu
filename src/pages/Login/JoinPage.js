5863d5b9842666a8a237d2616b4dd86d7d8e013f
SeungJi20 <lina6709@naver.com>
2023-11-04 04:47:05 +0900

    joinpage

--- src/pages/Login/JoinPage.js
+++ src/pages/Login/JoinPage.js
@@ -2,13 +2,12 @@ import React from "react";
 import Header from "../../components/Header/HeaderGuest";
 import styled from "styled-components";
 import { CONTAINER_WIDTH, HEADER_HEIGHT } from "../../assets/system/layout";
-import { GRAY } from "../../colors";
 import { useNavigate } from "react-router-dom";
+import Mentee from "../../assets/images/mentee.png";
+import Mentor from "../../assets/images/mentor.png";
 
 const JoinPage = () => {
   const navigate = useNavigate();
-
-
 
   const onBox1Click = () => {
     // Box1 클릭 시 수행할 작업 추가
@@ -20,24 +19,82 @@ const JoinPage = () => {
     navigate('/mentor/join');
     
   }
+  const onClickLoginButton = () => {
+    navigate('/user/login')
+  }
 
   return (
     <Root>
       <Header />
-      <Container>
+       <Container>
         CHAMedu
       </Container>
+      <Subtitle>
+        바른 교육의 출발, 지금 시작해보세요!
+      </Subtitle>
+      <SubTitleTTTT>당신은 어떤 분 인가요?</SubTitleTTTT>
+      
+      <BoxWrapper>
+        <Box1 onClick={onBox1Click}>
+          <BoxDiv>저는 상담을 통해<br/>입시정보를 알고싶은</BoxDiv>
+          <ImageWrapper src={Mentee} alt={"멘티"} />
+          <BoxDiv><span>멘티 </span>입니다.</BoxDiv>
+        </Box1>
 
-      <Box1 onClick={onBox1Click}>
-        내용1
-      </Box1>
+        <Box1 onClick={onBox2Click}>
+          <BoxDiv>저는 상담을 통해<br/>입시정보를 공유하고싶은</BoxDiv>
+          <ImageWrapper src={Mentor} alt={"멘토"} />
+          <BoxDiv><span>멘토 </span>입니다.</BoxDiv>
+        </Box1>
+      </BoxWrapper>
 
-      <Box2 onClick={onBox2Click}>
-        내용2
-      </Box2>
+      <AlreadySingup>
+        <div>이미 회원이신가요?</div>
+        <AlreadyButton onClick={onClickLoginButton}>로그인</AlreadyButton>
+      </AlreadySingup>
     </Root>
   );
 };
+
+const AlreadyButton = styled.div`
+    color: #fff;
+    background-color: #4CAF4F;
+    width: 42px;
+    border-radius: 8px;
+    padding: 10px 8px;
+`;
+
+const AlreadySingup = styled.div`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    width: 600px;
+    gap: 40px;
+    border-top: 1px solid gray;
+    margin-top: 40px;
+    padding-top: 40px;
+    margin-bottom: 200px;
+`;
+
+const BoxDiv = styled.div`
+    color: #000;
+  font-size: 15px;
+  font-weight: 700;
+  margin-bottom:20px;
+  text-align: center;
+
+  & span {
+    color: #4CAF4F;
+  }
+`;
+
+const BoxWrapper = styled.div`
+    display: inline-flex;
+    justify-content: center;
+    width: 100%;
+    gap: 40px;
+    margin-top: 30px;
+`;
 
 const Root = styled.div`
   width: 100%;
@@ -46,7 +103,6 @@ const Root = styled.div`
   align-items: center;
   padding-top: ${HEADER_HEIGHT}px;
   position: relative;
-  background-color: ${GRAY.LIGHT};
 `;
 
 const Container = styled.div`
@@ -60,22 +116,40 @@ const Container = styled.div`
   font-weight: bold;
 `;
 
-const Box1 = styled.div`
-  background-color: #E91E63;
-  color: #fff;
-  padding: 20px;
-  margin-top: 10px;
-  font-size: 16px;
-  cursor: pointer; // 커서 모양 변경 (선택 사항)
+const ImageWrapper = styled.img`
+  width: 250px;
+  height: 250px;
+  margin-bottom: 40px;
 `;
 
-const Box2 = styled.div`
-  background-color: #2196F3;
+const Subtitle = styled.div`
+  font-size: 16px;
+  color: #4CAF4F;
+  margin-top: 30px;
+  margin-bottom: 50px;
+`;
+
+const SubTitleTTTT = styled.div`
+  font-size: 20px;
+  font-weight: 800;
+`;
+
+const Box1 = styled.div`
   color: #fff;
   padding: 20px;
   margin-top: 10px;
   font-size: 16px;
   cursor: pointer; // 커서 모양 변경 (선택 사항)
+  border: 1px solid gray;
+  border-radius: 80px;
+  display: inline-flex;
+  flex-direction: column;
+  height: 100%;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 70px 50px;
 `;
+
 
 export default JoinPage;
--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -1,38 +1,86 @@
-import React from "react";
-import Header from "../../components/Header/HeaderGuest";
-import styled from "styled-components";
-import { CONTAINER_WIDTH, HEADER_HEIGHT } from "../../assets/system/layout";
-import { GRAY } from "../../colors";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import Header from '../../components/Header/HeaderGuest'
+import styled from 'styled-components'
+import { CONTAINER_WIDTH, HEADER_HEIGHT } from '../../assets/system/layout'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+  const [id, setId] = useState('')
+  const [pw, setPw] = useState('')
+
+  const handleIdChange = (event) => {
+    setId(event.target.value) // 입력된 값으로 상태를 업데이트합니다.
+  }
+  const handlePwChange = (event) => {
+    setPw(event.target.value)
+  }
 
   const onClickJoinButton = () => {
-    navigate('/user/join');
+    navigate('/user/join')
+  }
+
+  const [isChecked, setIsChecked] = useState(false)
+
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked)
   }
 
   return (
     <Root>
       <Header />
-      <Container>
-        CHAMedu
-      </Container>
-      <Subtitle>
-        바른 교육의 출발, 지금 시작해보세요!
-      </Subtitle>
-      
+      <Container>CHAMedu</Container>
+      <Subtitle>바른 교육의 출발, 지금 시작해보세요!</Subtitle>
+
       <LoginForm>
-        <InputField type="text" placeholder="아이디" />
-        <InputField type="password" placeholder="비밀번호" />
-        <ButtonSignup onClick={onClickJoinButton}>
-          회원가입
-        </ButtonSignup>
-        <LoginButton>로그인</LoginButton>
+        <InputField
+          type="text"
+          placeholder="아이디"
+          value={id}
+          onChange={handleIdChange}
+        />
+        <InputField
+          type="password"
+          placeholder="비밀번호"
+          value={pw}
+          onChange={handlePwChange}
+        />
+        <LabelTTTT>
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+          />
+          아이디 저장
+        </LabelTTTT>
+
+        <LoginButton
+          onClick={() => {
+            console.log('id : ', id)
+            console.log('pw : ', pw)
+          }}
+        >
+          로그인
+        </LoginButton>
+
+        <SignupTTTT>
+          <div>아직 회원이 아니세요?</div>
+          <ButtonSignup onClick={onClickJoinButton}>회원가입</ButtonSignup>
+        </SignupTTTT>
       </LoginForm>
     </Root>
-  );
-};
+  )
+}
+
+const SignupTTTT = styled.div`
+  border-top: 1px solid gray;
+      display: inline-flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    margin-top: 80px;
+    padding: 20px;
+`
 
 const Root = styled.div`
   width: 100%;
@@ -41,8 +89,8 @@ const Root = styled.div`
   align-items: center;
   padding-top: ${HEADER_HEIGHT}px;
   position: relative;
-  background-color: ${GRAY.LIGHT};
-`;
+  background-color: #fff;
+`
 
 const Container = styled.div`
   width: ${CONTAINER_WIDTH}px;
@@ -53,19 +101,20 @@ const Container = styled.div`
   margin-top: 10px;
   color: #4CAF4F;
   font-weight: bold;
-`;
+`
 
 const Subtitle = styled.div`
   font-size: 16px;
-  color: #333;
-  margin-top: 16px;
-`;
+  color: #4CAF4F;
+  margin-top: 30px;
+  margin-bottom: 50px;
+`
 
 const LoginForm = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
+`
 
 const InputField = styled.input`
   width: 300px;
@@ -74,18 +123,15 @@ const InputField = styled.input`
   border: 1px solid #ccc;
   border-radius: 4px;
   margin-top: 10px;
-}
-`;
+`
 
-const ButtonSignup = styled.div`
-  background-color: #4CAF4F;
-  color: #fff;
+const ButtonSignup = styled.div` 
+  color: #4CAF4F;
   width: 60px;
   padding: 10px 20px;
   border-radius: 6px;
-  margin-top: 10px; /* 수정된 부분: 비밀번호 아래로 이동 */
   font-size: 15px;
-`;
+`
 
 const LoginButton = styled.button`
   background-color: #4CAF4F;
@@ -97,6 +143,15 @@ const LoginButton = styled.button`
   margin-top: 20px;
   font-size: 16px;
   cursor: pointer;
-`;
+  width:100%;
+`
 
-export default LoginPage;
+const LabelTTTT = styled.label`
+  display: inline-flex;
+  width: 100%;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  justify-content: flex-end;
+`
+
+export default LoginPage
