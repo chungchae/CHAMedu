eff6354f5fe2206d2721adf9ecd9eea8494ce380
chungchae <chungchae0@gmail.com>
2023-12-04 00:25:17 +0900

    api: Mentor MyPage 상담 신청 목록, 수락, 신청 연결

--- src/components/Mentor/Consultation/RequestContent.js
+++ src/components/Mentor/Consultation/RequestContent.js
@@ -1,55 +1,104 @@
 import styled from "styled-components";
-import React from 'react';
+import React from "react";
 import Person from "../../../assets/images/mypage_person.png";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Button, ConfigProvider, Typography } from "antd";
+import { PRIMARY } from "../../../colors";
 
 const RequestContent = () => {
-  const imageList = [
-    {
-      imageName: Person,
-      name: "이한별",
-      date: '2023-11-14',
-      time: '13:00~13:30',
-      title: '동국대학교 논술 문제유형관련 질문',
-      onAccept: () => console.log('Accepted 1'),
-      onReject: () => console.log('Rejected 1'),
-    },
-    {
-      imageName: Person,
-      name: "홍길동",
-      date: '2023-12-14',
-      time: '20:00~20:30',
-      title: '논술 수학 범위 관련 질문',
-      onAccept: () => console.log('Accepted 1'),
-      onReject: () => console.log('Rejected 1'),
-    },
-    
-  ];
-    return (
-        <>
-         <RoundedBox>
-            <HeaderText>상담 수락 대기 목록</HeaderText>
-            {imageList.map((image, index) => (
-              <RequestWrapper key={index}>
-                <RequestUserWrapper>
-                  <RequestImageWrapper>
-                    <RequestImage src={Person} alt="Image"/>
-                    <div>{image.name}</div>
-                  </RequestImageWrapper>
-                  <div>{image.date}</div>
-                  <div>{image.time}</div>
-                  <div>{image.title}</div>
-                </RequestUserWrapper>
+  const [requestData, setRequestData] = useState([]);
 
-                <RequestButtonWrapper>
-                  <RequestButton1 onClick={image.onAccept}>수락</RequestButton1>
-                  <RequestButton2 onClick={image.onReject}>거절</RequestButton2>
-                </RequestButtonWrapper>
-              </RequestWrapper>  
-            ))}
-          </RoundedBox>
-          </>
-    );
-}
+  //상담 신청 목록 API
+  const getChatRequestData = () => {
+    axios
+      .get("/api/mentor-mypage/chat-request", { withCredentials: true })
+      .then((res) => {
+        console.log("신청데이터:", res);
+        setRequestData(res.data);
+      })
+      .catch((error) => {
+        console.error("Axios Error:", error);
+        console.log("Full Axios Response:", error.response);
+      });
+  };
+
+  //상담 수락 API
+  const acceptChatRequest = (roomId) => {
+    const requestData = { roomId, answer: true };
+    axios
+      .post("/api/mentor-mypage/chat-request/answer", requestData, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log("상담 수락 성공:", res);
+      })
+      .catch((error) => {
+        console.error("상담 수락 실패:", error);
+        console.log("Full Axios Response:", error.response);
+      });
+  };
+
+  //상담 거절 API
+  const refuseChatRequest = (roomId) => {
+    const requestData = { roomId, answer: false };
+    axios
+      .post("/api/mentor-mypage/chat-request/answer", requestData, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log("상담 거절 성공:", res);
+      })
+      .catch((error) => {
+        console.error("상담 거절 실패:", error);
+        console.log("Full Axios Response:", error.response);
+      });
+  };
+
+  useEffect(() => {
+    getChatRequestData();
+  }, []);
+
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: PRIMARY.DEFAULT,
+        },
+      }}
+    >
+      <RoundedBox>
+        <HeaderText>상담 수락 대기 목록</HeaderText>
+        {requestData.map((request, index) => (
+          <RequestWrapper key={index}>
+            <RequestUserWrapper>
+              <RequestImageWrapper>
+                <RequestImage src={Person} alt='Image' />
+                <NameTypo>{request.menteeName}</NameTypo>
+              </RequestImageWrapper>
+              <ContentTypo>{request.startDate}</ContentTypo>
+              <ContentTypo>{request.durationTime}</ContentTypo>
+              <ContentTypo>{request.chatTitle}</ContentTypo>
+            </RequestUserWrapper>
+
+            <RequestButtonWrapper>
+              <RequestButton1
+                type='primary'
+                onClick={() => acceptChatRequest(request.roomId)}
+              >
+                수락
+              </RequestButton1>
+              <RequestButton2 onClick={() => refuseChatRequest(request.roomId)}>
+                거절
+              </RequestButton2>
+            </RequestButtonWrapper>
+          </RequestWrapper>
+        ))}
+      </RoundedBox>
+    </ConfigProvider>
+  );
+};
 
 const RoundedBox = styled.div`
   background-color: white;
@@ -57,65 +106,68 @@ const RoundedBox = styled.div`
   width: calc(100% - 150px);
   margin-top: 10px;
   margin-left: 50px;
-  padding-right: 50px;
+  padding: 20px;
   border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 const HeaderText = styled.div`
-  font-size: 14px;
-  padding-top: 30px;
+  width: 100%;
+  font-size: 18px;
+  padding-top: 20px;
   padding-bottom: 30px;
   padding-left: 20px;
   font-family: "esamanru";
 `;
 
+const NameTypo = styled(Typography)`
+  width: 80px;
+  font-size: 15px;
+`;
 
-const RequestButton2 = styled.div`
-  font-family: "esamanru";
-  cursor: pointer;
-  background-color: #C4C4C4;
-  padding: 10px;
-  color: #fff;
+const ContentTypo = styled(Typography)`
+  font-size: 15px;
+`;
+
+const RequestButton2 = styled(Button)`
   border-radius: 15px;
 `;
 
-const RequestButton1 = styled.div`
-  font-family: "esamanru";
-  cursor: pointer;
-  background-color: #89CC8B;
-  padding: 10px;
-  color: #fff;
+const RequestButton1 = styled(Button)`
   border-radius: 15px;
 `;
 
 const RequestImageWrapper = styled.div`
-    display: inline-flex;
-    align-items: center;
+  display: inline-flex;
+  align-items: center;
 `;
 
 const RequestUserWrapper = styled.div`
-    display: inline-flex;
-    align-items: center;
-    gap:30px;
-
+  display: inline-flex;
+  align-items: center;
+  gap: 30px;
 `;
 
 const RequestButtonWrapper = styled.div`
   display: inline-flex;
-    gap: 10px;
+  gap: 10px;
 `;
 
 const RequestImage = styled.img``;
 
 const RequestWrapper = styled.div`
-    display: inline-flex;
-    align-items: center;
-    width: 100%;
-    border: 1px solid #ccc;
-    border-radius: 20px;
-    margin: 5px;
-    padding: 20px;
-    justify-content: space-between;
-    font-weight: 900;
+  display: inline-flex;
+  align-items: center;
+  width: calc(100% - 120px);
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  margin: 5px;
+  padding: 20px;
+  margin-bottom: 10px;
+  justify-content: space-between;
+  font-weight: 900;
 `;
 
 export default RequestContent;
