4dee43a67e08a0d1cef89caf212ab205a29fc7b3
SeungJi20 <lina6709@naver.com>
2023-12-06 23:49:42 +0900

    feat: menteemodifymodal 수정

--- src/components/Mentor/MenteeModifyModal.js
+++ src/components/Mentor/MenteeModifyModal.js
@@ -23,18 +23,10 @@ const MenteeModifyModal = ({ mentorData, isOpen, closeModal }) => {
   const [admissionSelect, setAdmissionSelect] = useState(""); //전형 옵션
   const [collegeSelect, setCollegeSelect] = useState(""); //단과대 옵션
 
-  const [startDate, setStartDate] = useState(new Date()); //가능 시간 선택
-  const [selectedTime, setSelectedTime] = useState(null);
-  const [selectedTimes, setSelectedTimes] = useState([]); //가능한 시간 리스트
 
   const data = { //API에 전송할 data 정의
     nickName: nickname,
-    promotionText: promotionText,
-    userImg: null,
-    university: university,
-    admissionType: admissionSelect,
-    college: collegeSelect,
-    availableTime: selectedTimes,
+    info: promotionText,
   };
 
   const MentorModifyApi = (data) => {
@@ -54,42 +46,6 @@ const MenteeModifyModal = ({ mentorData, isOpen, closeModal }) => {
       });
   };
 
-  const admissionOptions = [
-    { value: 0, label: "학종" },
-    { value: 1, label: "정시" },
-    { value: 2, label: "교과" },
-    { value: 3, label: "논술" },
-  ];
-  const collegeOptions = [
-    { value: 0, label: "공과대" },
-    { value: 1, label: "자연대" },
-    { value: 2, label: "문과대" },
-    { value: 3, label: "예대" },
-    { value: 4, label: "체대" },
-  ];
-
-  const timeOptions = [
-    "09:00",
-    "09:30",
-    "10:00",
-    "10:30",
-    "11:00",
-    "11:30",
-    "12:00",
-    "12:30",
-    "13:00",
-    "13:30",
-    "14:00",
-    "14:30",
-    "15:00",
-    "15:30",
-    "16:00",
-    "16:30",
-    "17:00",
-    "17:30",
-    "18:00",
-  ];
-
   useEffect(() => {
     console.log("멘토데이터!!", mentorData);
     setNickname(mentorData.nickname || "");
@@ -100,36 +56,7 @@ const MenteeModifyModal = ({ mentorData, isOpen, closeModal }) => {
     setCollegeSelect(mentorData.university || "");
   }, [mentorData]);
 
-  const handleAdmissionChange = (option) => {
-    setAdmissionSelect(option);
-  };
-  const handleCollegeChange = (option) => {
-    setCollegeSelect(option);
-  };
 
-  const handleTimeButtonClick = (time) => {
-    setStartDate((prevDate) => {
-      const newDate = new Date(prevDate);
-      const [hours, minutes] = time.split(":");
-      newDate.setHours(parseInt(hours, 10));
-      newDate.setMinutes(parseInt(minutes, 10));
-
-      // Toggle the selection of the time slot
-      setSelectedTimes((prevSelectedTimes) => {
-        if (prevSelectedTimes.includes(time)) {
-          // Deselect the time slot
-          return prevSelectedTimes.filter(
-            (selectedTime) => selectedTime !== time
-          );
-        } else {
-          // Select the time slot
-          return [...prevSelectedTimes, time];
-        }
-      });
-
-      return newDate;
-    });
-  };
 
   return (
     <ConfigProvider
@@ -154,54 +81,15 @@ const MenteeModifyModal = ({ mentorData, isOpen, closeModal }) => {
                   value={nickname}
                   onChange={(e) => setNickname(e.target.value)}
                 />
-                <InfoTypo>학력</InfoTypo>
-                <InfoInput
-                  value={university}
-                  onChange={(e) => setUniversity(e.target.value)}
-                />
                 <InfoTypo>자기소개</InfoTypo>
                 <ContentInput
                   value={promotionText}
                   onChange={(e) => setPromotionText(e.target.value)}
                 />
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                  <SelectContainer>
-                    <InfoTypo>입시 전형 선택</InfoTypo>
-                    <SelectBox
-                      value={admissionSelect}
-                      onChange={(option) => handleAdmissionChange(option)}
-                      placeholder='입시 전형 선택'
-                      options={admissionOptions}
-                    />
-                  </SelectContainer>
-                  <SelectContainer>
-                    <InfoTypo>단과대 선택</InfoTypo>
-                    <SelectBox
-                      /* value={admissionSelect} */
-                      onChange={(option) => handleCollegeChange(option)}
-                      placeholder='단과대 선택'
-                      options={collegeOptions}
-                    />
-                  </SelectContainer>
-                </div>
+               
               </InfoContainer2>
             </InfoContainer>
-            <TimeContainer>
-              <InfoTypo style={{ marginLeft: "7px" }}>
-                상담 가능 시간 선택
-              </InfoTypo>
-              <TimeSelection>
-                {timeOptions.map((time) => (
-                  <TimeButton
-                    key={time}
-                    onClick={() => handleTimeButtonClick(time)}
-                    selected={selectedTimes.includes(time)}
-                  >
-                    {time}
-                  </TimeButton>
-                ))}
-              </TimeSelection>
-            </TimeContainer>
+            
             <ModifyButton onClick={() => MentorModifyApi(data)}>
               프로필 수정하기
             </ModifyButton>
@@ -246,6 +134,7 @@ const Container = styled.div`
 `;
 
 const InfoContainer = styled.div`
+    margin-top: 100px;
   display: flex;
   flex-direction: row;
   align-items: center;
