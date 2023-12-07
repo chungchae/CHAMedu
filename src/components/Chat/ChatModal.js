53d6cc17583987be464cff81ce945b97fd87f0f3
chungchae <chungchae0@gmail.com>
2023-12-07 11:02:30 +0900

    style 수정, chatModal 제작중

--- src/components/Chat/ChatModal.js
+++ src/components/Chat/ChatModal.js
@@ -8,17 +8,35 @@ import { useHistory } from 'react-router';
 import io from 'socket.io-client';
 import axios from "axios";
 
-const ChatModal = ({ isOpen, closeModal }) => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [selectedTime, setSelectedTime] = useState(null);
-  const [timeList, setTimeList] = useState();
+const ChatModal = ({ isOpen, closeModal, roomId }) => {
+  const [messageList, setMessageList] = useState([]);
+
+   const socket = io.connect('http://localhost:8080');
+
+  /* useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+    console.log(socket)
+  }, [socket]); */
+
+  socket.on("connect", () => {
+    console.log("socket server connected.");
+  });
+  
+  socket.on("disconnect", () => {
+    console.log("socket server disconnected.");
+  });
+  useEffect(() => {
+    console.log('채팅방 아이디:',roomId)
+  }, [])
 
   return (
     <Modal isOpen={isOpen} onRequestClose={closeModal} style={Modalstyle}>
       <Root>
         <CloseButton onClick={closeModal}>X</CloseButton>
         <Container>
-          
+          <TitleTypo>상담 채팅방</TitleTypo>
         </Container>
       </Root>
     </Modal>
@@ -56,41 +74,13 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-const DateContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 83%;
-`;
-
-const TimeContainer = styled.div``;
-
-const ProfileContainer = styled.div`
-  width: 90%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  padding: 40px 0px;
-`;
-
-const ProfileContainer2 = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const TimeSelection = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin-bottom: 10px;
-`;
-
-const DatePickerContainer = styled.div`
-  height: 220px;
-  margin-right: 50px;
+const TitleTypo = styled(Typography)`
+  font-family: "esamanru";
+  font-size: 18px;
+  span {
+    padding-right: 1px;
+    font-weight: 700;
+  }
 `;
 
 const CloseButton = styled.button`
@@ -105,77 +95,6 @@ const CloseButton = styled.button`
 
   &:hover {
     color: ${PRIMARY.LIGHT};
-  }
-`;
-
-const MentorProfileImg = styled.img`
-  width: 250px;
-  height: 125px;
-  object-fit: cover;
-  border-radius: 200px;
-  padding: 0px 30px;
-  padding-bottom: 20px;
-`;
-
-const TimeButton = styled.button`
-  margin: 5px;
-  padding: 8px;
-  background-color: ${(props) => (props.selected ? PRIMARY.LIGHT : "white")};
-  color: ${(props) => (props.selected ? "white" : "black")};
-  border: 1px solid ${PRIMARY.LIGHT};
-  border-radius: 20px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${PRIMARY.LIGHT};
-    color: white;
-  }
-`;
-
-const SelectedTime = styled.div`
-  font-size: 16px;
-`;
-
-
-const TitleTypo = styled(Typography)`
-  font-family: "esamanru";
-  font-size: 18px;
-  span {
-    padding-right: 1px;
-    font-weight: 700;
-  }
-`;
-
-const EduTypo = styled(Typography)`
-  font-family: "esamanru";
-  font-size: 16px;
-  color: ${GRAY.DARK};
-`;
-
-const InfoTypo = styled(Typography)`
-  font-family: "esamanru";
-  font-size: 16px;
-  padding-right: 20px;
-`;
-
-const MenuTypo = styled(Typography)`
-  font-family: "esamanru";
-  font-size: 18px;
-`;
-
-const ReserveButtonStyled = styled.button`
-  background-color: ${PRIMARY.DEFAULT};
-  color: white;
-  padding: 10px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  position: absolute;
-  bottom: 20px; 
-  right: 20px; 
-
-  &:hover {
-    background-color: ${PRIMARY.LIGHT};
   }
 `;
 
