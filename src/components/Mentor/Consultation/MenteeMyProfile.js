058131e72fe6e8b6d5b1c64e3af26744c1fa36fb
SeungJi20 <lina6709@naver.com>
2023-12-12 01:46:06 +0900

    feat: MenteeMyProfile 변경된 정보 적용 및 희망 대학교 희망 단과대 입시전형 보이기 완료

--- src/components/Mentor/Consultation/MenteeMyProfile.js
+++ src/components/Mentor/Consultation/MenteeMyProfile.js
@@ -1,30 +1,60 @@
 import styled from "styled-components";
 import React, { useState, useEffect } from "react";
-import { Button, Typography, Tag, ConfigProvider } from "antd";
-import NoteIcon from "../../../assets/images/note_icon.png";
+import { Button, Typography, Tag } from "antd";
 import { CONTAINER_WIDTH } from "../../../assets/system/layout";
 import { GRAY, PRIMARY } from "../../../colors";
 import Person from "../../../assets/images/mypage_person.png";
 import axios from "axios";
 import MenteeModifyModal from "../../../components/Mentor/MenteeModifyModal";
-import ChatModal from "../../Chat/ChatModal";
 
 const MenteeMyProfile = () => {
-  const [menteeData, setMenteeData] = useState({});
+  const [menteeData, setMenteeData] = useState({}); //데이터 저장할 곳 만듦
   const [modalModifyOpen, setModalModifyOpen] = useState(false);
-  const [chatModalOpen, setChatModalOpen] = useState(false);
-
-  //세션에서 userId get
   const userId = sessionStorage.getItem("userId");
 
-  //멘티 마이프로필 데이터 GET API
+  const getAdmissionColor = (value) => {
+    if (value === 3) {
+      return '#99DDEC';
+    }
+    if (value === 0) {
+      return '#E9E587';
+    }
+    if (value === 2) {
+      return '#F8A7A2';
+    }
+    if (value === 1) {
+      return '#D5BEED';
+    }
+    else {
+      return '#C4C4C4'
+    }
+  };
+  const translateAdmission = (value) => {
+      if (value === 0) {
+        return "학종";
+      }
+      if (value === 1) {
+        return "정시";
+      }
+      if (value === 2) {
+        return "교과";
+      }
+      if (value === 3) {
+        return "논술";
+      }
+      else {
+        return "기타";
+      }
+    };
+
   useEffect(() => {
+    //api 부름
     const getMentee = () => {
       axios
         .get(`http://localhost:8080/api/mentee-mypage/${userId}`)
         .then((res) => {
           console.log(res);
-          setMenteeData(res.data);
+          setMenteeData(res.data); // 아까 거기에 저장
         })
         .catch((error) => {
           console.error("Axios Error", error);
@@ -33,7 +63,6 @@ const MenteeMyProfile = () => {
     getMentee();
   }, []);
 
-  //수정 모달창 관리
   const openModifyModal = () => {
     setModalModifyOpen(true);
   };
@@ -41,26 +70,22 @@ const MenteeMyProfile = () => {
     setModalModifyOpen(false);
   };
 
-  //채팅 모달창 관리
-  const openChatModal = () => {
-    setChatModalOpen(true);
-  };
-  const closeChatModal = () => {
-    setChatModalOpen(false);
-  };
+  const collegeChange = (wish) => {
+    if(wish === 0){
+      return "공과대";
+    } else if (wish === 1){
+      return "자연대";
+    } else if (wish === 2){
+      return "문과대";
+    }else if (wish === 3){
+      return "예대";
+    }else {
+      return "체대";
+    }
+      
+  }
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: PRIMARY.DEFAULT,
-        },
-      }}
-    >
-      {/* <Time>
-        <IconImg src={NoteIcon} />
-        <div>{menteeData?.currentChatTime}</div>
-        <TimeSpan>확인하기</TimeSpan>
-      </Time> */}
+    <>
       <Container>
         <Profilecontainer>
           <Infocontainer>
@@ -77,7 +102,15 @@ const MenteeMyProfile = () => {
                   프로필 수정하기
                 </ModifyButton>
               </MentorNameTypo>
+            <DepartmentTag
+                color={getAdmissionColor(menteeData?.admissionType)}
+              >
+                {translateAdmission(menteeData?.admissionType)}
+              </DepartmentTag>
             </div>
+
+            <MentorEducationTypo>
+              희망 대학교 : {menteeData?.university} <Gap /> 희망 단과대 : {collegeChange(menteeData?.wishCollege)}</MentorEducationTypo>
             <MentorIntroTypo>{menteeData?.promotionText}</MentorIntroTypo>
             <ButtonContainer>
               <MenteeModifyModal
@@ -95,10 +128,9 @@ const MenteeMyProfile = () => {
               <RequestWrapper key={index}>
                 <RequestUserWrapper>
                   <RequestImageWrapper>
-                    <RequestImage src={Person} alt='Image' />
+                    <RequestImage src={Person} alt="Image" />
                     <div>{room.mentorName}</div>
                   </RequestImageWrapper>
-
                   <div>{room.startTime.split("T")[0]}</div>
                   <div>
                     {room.startTime.split("T")[1].substring(0, 5)} ~{" "}
@@ -117,33 +149,38 @@ const MenteeMyProfile = () => {
                     <RequestButton1>대기상태</RequestButton1>
                   )}
                   {room.status === "A" && (
-                    <RequestButton1 onClick={openChatModal}>
-                      채팅조회
-                    </RequestButton1>
+                    <RequestButton1>예정상태</RequestButton1>
                   )}
                   {room.status === "C" && <RequestButton1>완료</RequestButton1>}
                   {room.status === "D" && (
                     <RequestButton1>요청됨</RequestButton1>
                   )}
                 </RequestButtonWrapper>
-                <ChatModal
-                  isOpen={chatModalOpen}
-                  closeModal={closeChatModal}
-                  roomId={room.roomId}
-                />
               </RequestWrapper>
             ))}
         </RoundedBox>
       </Container>
-    </ConfigProvider>
+    </>
   );
 };
 
+const Gap = styled.div`
+  width: 30px;
+`;
+
 const ModifyButton = styled(Button)`
   position: absolute;
-  bottom: 30px;
-  right: 30px;
-  font-family: "esamanru";
+  margin-left: 200px;
+  margin-top: 0px;
+  color: ${PRIMARY.DEFAULT};
+  &:hover {
+    color: ${PRIMARY.DEFAULT}!important;
+    border-color: ${PRIMARY.DEFAULT}!important;
+  }
+  &:focus {
+    color: ${PRIMARY.DEFAULT}!important;
+    border-color: ${PRIMARY.DEFAULT}!important;
+  }
 `;
 
 const ButtonContainer = styled.div`
@@ -156,9 +193,6 @@ const Time = styled.div`
   margin-left: 30px;
   margin-top: 20px;
   font-family: "esamanru";
-  display: flex;
-  flex-direction: row;
-  align-items: center;
 `;
 
 const TimeSpan = styled.span`
@@ -182,8 +216,6 @@ const Profilecontainer = styled.div`
   margin: 30px 0px;
   border-radius: 20px;
   padding: 30px;
-  position: relative;
-  margin-top: 15px;
 `;
 
 const Infocontainer = styled.div`
@@ -204,6 +236,7 @@ const MentorEducationTypo = styled(Typography)`
   font-family: "esamanru";
   color: ${GRAY.DARK};
   padding-bottom: 7px;
+  display: inline-flex;
 `;
 
 const MentorIntroTypo = styled(Typography)`
@@ -236,7 +269,7 @@ const HeaderText = styled.div`
   font-family: "esamanru";
 `;
 
-const RequestButton1 = styled(Button)`
+const RequestButton1 = styled.div`
   font-family: "esamanru";
   background-color: #e9e9e9;
   padding: 12px; /* Adjust padding as needed */
@@ -277,6 +310,15 @@ const RequestWrapper = styled.div`
   margin-left: 30px;
   justify-content: space-between;
   font-weight: 900;
+`;
+const DepartmentTag = styled(Tag)`
+  display: flex;
+  align-items: center;
+  height: 27px;
+  border: none;
+  font-size: 14px;
+  color: white;
+  font-family: "esamanru";
 `;
 
 export default MenteeMyProfile;
